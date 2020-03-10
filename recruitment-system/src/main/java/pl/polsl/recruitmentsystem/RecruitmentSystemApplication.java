5badafbb73b39b53a5035d52d21c3fc2cd67a5af
Kamil Susek <dumacataloni@gmail.com>
2020-03-10 19:09:53 +0100

    Konfiguracja środowiska.

---
+++ recruitment-system/src/main/java/pl/polsl/recruitmentsystem/RecruitmentSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.recruitmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentSystemApplication.class, args);
+	}
+
+}
---
+++ recruitment-system/src/test/java/pl/polsl/recruitmentsystem/RecruitmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.recruitmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
