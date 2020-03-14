cc1fe1d2d6384bfbaba326c652eb937100da5caa
wanakubwa <adamski-piotrek@wp.pl>
2020-03-14 23:48:04 +0100

    kdiff FB

--- recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/service/RecruiterService.java
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/service/RecruiterService.java
@@ -13,7 +13,7 @@ public interface RecruiterService {
 
     List<Recruiter> findByFirstName(String firstName);
 
-    List<Recruiter> findBySecondNameTEMPCHANGE3(String secondName);
+    List<Recruiter> findBySecondNameTEMPCHANGE4(String secondName);
 
     Recruiter save(Recruiter recruiter);
 
