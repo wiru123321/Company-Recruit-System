495196300ee393a72f1248dbc6bba09a42140846
KamilSusek <dumacataloni@gmail.com>
2020-03-14 23:10:29 +0100

    Modyfikacja popupu logowania.

--- recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/FirstController.java
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/FirstController.java
@@ -1,6 +1,7 @@
 package com.polsl.proj.recruitmentsystem;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,7 +13,7 @@ public class FirstController {
         return "main";
     }
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     String userWelcome() {
         return "Welcome";
     }
---
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/service/RecruiterService.java
@@ -0,0 +1,24 @@
+package com.polsl.proj.recruitmentsystem.service;
+
+import com.polsl.proj.recruitmentsystem.model.Recruiter;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface RecruiterService {
+
+    Collection<Recruiter> findAll();
+
+    Recruiter findById(Long id);
+
+    List<Recruiter> findByFirstName(String firstName);
+
+    List<Recruiter> findBySecondName(String secondName);
+
+    Recruiter save(Recruiter recruiter);
+
+    Recruiter update(Recruiter recruiter);
+
+    Recruiter deleteById(Long id);
+
+}
