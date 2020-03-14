4ebebb2fcba60b4400a7edd845acf79fd6f08eec
wanakubwa <wanakubwa@gmail.com>
2020-03-14 19:36:06 +0100

    Konfiguracja postgresql.

--- recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/FirstController.java
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/FirstController.java
@@ -2,12 +2,18 @@ package com.polsl.proj.recruitmentsystem;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/main")
-    String mainPage(){
+    String mainPage() {
         return "main";
+    }
+
+    @PostMapping("/login")
+    String userWelcome() {
+        return "Welcome";
     }
 }
--- recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/model/HeadRecruiter.java
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/model/HeadRecruiter.java
@@ -1,4 +1,46 @@
 package com.polsl.proj.recruitmentsystem.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class HeadRecruiter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public HeadRecruiter(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
