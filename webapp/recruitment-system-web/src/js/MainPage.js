5225a3a6a33c8aad91e814148045fe287b36b519
besrei <tomek.wienchor@gmail.com>
2020-03-19 16:54:59 +0100

    Nowa strona główna. Dodano pole login oraz opis programu.

--- webapp/recruitment-system-web/src/js/MainPage.js
+++ webapp/recruitment-system-web/src/js/MainPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../css/MainPage.css';
 import '../css/LoginPopup.css';
 import Popup from 'reactjs-popup';
-import axios from 'axios';
+import network from '../resources/network.png';
+import idea from '../resources/idea.png';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,8 +13,6 @@ import {
   useParams,
   useHistory,
 } from 'react-router-dom';
-
-const API = 'https://localhost:8080/recruiter/all';
 
 class MainPage extends React.Component {
   constructor (props) {
@@ -31,50 +30,86 @@ class MainPage extends React.Component {
   render () {
     return (
       <div>
-
         <div>
           <ul className="navBar">
-
             <li className="navBar navBarToLeft">
-              <Link to="/main">tmpmain</Link>
+              <p className="appTitle">
+                System Obsługi Rekrutacji
+              </p>
             </li>
-            <li className="navBar navBarToLeft">
-              <Link to="/recruiter">tmprecruiter</Link>
-            </li>
-            <li className="navBar navBarToLeft">
-              <Link to="/head">tmphead</Link>
-            </li>
-
             <li className="navBar navBarToRight">
-              <Login />
+              <form>
+                <li className="navBar navBarToRight">
+                  <button className="loginButton">Zaloguj się</button>
+                </li>
+                <li className="navBar navBarToRight">
+                  <input
+                    className="loginInput"
+                    placeholder="hasło"
+                    type="password"
+                  />
+                </li>
+                <li className="navBar navBarToRight">
+                  <input
+                    className="loginInput"
+                    placeholder="login"
+                    type="login"
+                  />
+                </li>
+              </form>
             </li>
+            <li className="navBar navBarToRight" />
           </ul>
         </div>
         <div className="pageContent">
-          /* TODO: Wstawic slider wyswietlajacy jakas zawartosc.*/
 
-          <div className="slide">
-            <button className="prev">prev</button>
-            <button className="next">next</button>
-          </div>
-          <a className="dot" />
-          <a className="dot" />
-          <a className="dot" />
-          <div>
-            <p className="footerSection" />
-            <footer className="footerSection" />
-          </div>
+          <p className="mainContent">
+            <br />
+            <p style={{fontWeight: 'bold'}}>O programie</p>
+            <br />
+            Proces rekrutacji jest związany z gromadzeniem znacznej ilości danych związanych głównie
+            z nadesłanymi CV. Aplikacja wspiera duże oraz małe przedsiębiorstwa pomagając
+            zarządzać całym etapem rekrutacji odciążając tym samym zespół HR. Należy również
+            zadbać o bezpieczeństwo danych osobowych. Obecnie wykorzystywanie wyłącznie arkuszy
+            kalkulacyjnych czy e-maili odchodzi w przeszłość. Informacje dotyczące kandydatów
+            powinny być dla pracowników HR przejrzyste, co usprawni proces rekrutacji. Istotne jest to
+            zwłaszcza w przypadku, kiedy mamy do czynienia z dużą liczbą aplikacji na dane
+            stanowisko.
+          </p>
+          <img className="networkImg" src={network} />
+          <img className="networkImg " src={idea} />
+
+          <p className="mainContent">
+            <br />
+            <p style={{fontWeight: 'bold'}}> Automatyzacja procesów</p>
+            <br />
+            Aplikacja wychodzi naprzeciw oczekiwaniom, które stawiane są przez zespoły HR trudniące
+            się rekrutacją w szczególności na dużą skalę. Wszystkie czynności wykonywane podczas
+            typowej rekrutacji zostały maksymalnie zautomatyzowane przez co rekruterzy mogą skupić
+            się na najważniejszej rzeczy jaką jest sam kandydat nie martwiąc się o stosy dokumentów.
+          </p>
+
+          <p className="mainContent">
+            <br />
+            <p style={{fontWeight: 'bold'}}> Pomoc w legalizacji pracy </p>
+            <br />
+
+            Najważniejszą rzeczą podczas rekrutacji w szególności tej owocnej jest podpisanie
+            stosownej umowy z kandydatem. Ten proces również został zautomatyzowany poprzez
+            zastosowanie systemu “Thru-Sync”, który umożliwia przesłanie umowy bezpośrednio do
+            kandydata.
+          </p>
+        </div>
+
+        <div>
+          <footer className="footerSection">
+            <br /><br />© 2020 - System Obsługi Rekrutacji
+          </footer>
         </div>
       </div>
     );
   }
 }
-
-const user = {
-  rlogin: 'rekruter',
-  hrlogin: 'hrekruter',
-  pswd: 'admin',
-};
 
 class Login extends React.Component {
   constructor (props) {
@@ -95,7 +130,11 @@ class Login extends React.Component {
   render () {
     return (
       <Popup
-        trigger={<button className="loginButton"> Zaloguj się </button>}
+        trigger={
+          <button className="loginButton">
+            Zaloguj się
+          </button>
+        }
         onOpen={event => {
           console.log (document.getElementsByClassName ('slide').length);
         }}
@@ -106,20 +145,9 @@ class Login extends React.Component {
           <form className="loginForm">
             <h1 className="loginForm"> Podaj dane logowania.</h1>
             <p className="loginForm" />
-            <input
-              value={login}
-              className="loginForm"
-              type="login"
-              placeholder="login"
-              onChange={}
-            />
+            <input className="loginForm" type="login" placeholder="login" />
             <br />
-            <input
-              value={pswd}
-              className="loginForm"
-              type="password"
-              placeholder="hasło"
-            />
+            <input className="loginForm" type="password" placeholder="hasło" />
             <br />
             <p className="loginForm" />
             <button className="loginForm" onClick={this.handleLoginClick}>
