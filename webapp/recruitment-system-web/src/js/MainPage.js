06d907ff538c1d74561b96f9092f691bdef128f9
besrei <tomek.wienchor@gmail.com>
2020-03-20 11:59:17 +0100

    Poprawiono układ tekstu na stronie głównej oraz pozycje obrazków.

--- webapp/recruitment-system-web/src/js/MainPage.js
+++ webapp/recruitment-system-web/src/js/MainPage.js
@@ -4,6 +4,7 @@ import '../css/LoginPopup.css';
 import Popup from 'reactjs-popup';
 import network from '../resources/network.png';
 import idea from '../resources/idea.png';
+import help from '../resources/help.png';
 import {
   BrowserRouter as Router,
   Switch,
@@ -62,43 +63,50 @@ class MainPage extends React.Component {
           </ul>
         </div>
         <div className="pageContent">
+          <div>
+            <p className="mainContent">
+              <br />
+              <p style={{fontWeight: 'bold'}}>O programie</p>
+              <br />
+              Proces rekrutacji jest związany z gromadzeniem znacznej ilości danych związanych głównie
+              z nadesłanymi CV. Aplikacja wspiera duże oraz małe przedsiębiorstwa pomagając
+              zarządzać całym etapem rekrutacji odciążając tym samym zespół HR. Należy również
+              zadbać o bezpieczeństwo danych osobowych. Obecnie wykorzystywanie wyłącznie arkuszy
+              kalkulacyjnych czy e-maili odchodzi w przeszłość. Informacje dotyczące kandydatów
+              powinny być dla pracowników HR przejrzyste, co usprawni proces rekrutacji. Istotne jest to
+              zwłaszcza w przypadku, kiedy mamy do czynienia z dużą liczbą aplikacji na dane
+              stanowisko.
 
-          <p className="mainContent">
-            <br />
-            <p style={{fontWeight: 'bold'}}>O programie</p>
-            <br />
-            Proces rekrutacji jest związany z gromadzeniem znacznej ilości danych związanych głównie
-            z nadesłanymi CV. Aplikacja wspiera duże oraz małe przedsiębiorstwa pomagając
-            zarządzać całym etapem rekrutacji odciążając tym samym zespół HR. Należy również
-            zadbać o bezpieczeństwo danych osobowych. Obecnie wykorzystywanie wyłącznie arkuszy
-            kalkulacyjnych czy e-maili odchodzi w przeszłość. Informacje dotyczące kandydatów
-            powinny być dla pracowników HR przejrzyste, co usprawni proces rekrutacji. Istotne jest to
-            zwłaszcza w przypadku, kiedy mamy do czynienia z dużą liczbą aplikacji na dane
-            stanowisko.
-          </p>
-          <img className="networkImg" src={network} />
-          <img className="networkImg " src={idea} />
+            </p>
+            <img className="networkImg" src={network} />
+          </div>
+          <div>
+            <p className="mainContent">
 
-          <p className="mainContent">
-            <br />
-            <p style={{fontWeight: 'bold'}}> Automatyzacja procesów</p>
-            <br />
-            Aplikacja wychodzi naprzeciw oczekiwaniom, które stawiane są przez zespoły HR trudniące
-            się rekrutacją w szczególności na dużą skalę. Wszystkie czynności wykonywane podczas
-            typowej rekrutacji zostały maksymalnie zautomatyzowane przez co rekruterzy mogą skupić
-            się na najważniejszej rzeczy jaką jest sam kandydat nie martwiąc się o stosy dokumentów.
-          </p>
+              <br />
+              <p style={{fontWeight: 'bold'}}> Automatyzacja procesów</p>
+              <br />
+              Aplikacja wychodzi naprzeciw oczekiwaniom, które stawiane są przez zespoły HR trudniące
+              się rekrutacją w szczególności na dużą skalę. Wszystkie czynności wykonywane podczas
+              typowej rekrutacji zostały maksymalnie zautomatyzowane przez co rekruterzy mogą skupić
+              się na najważniejszej rzeczy jaką jest sam kandydat nie martwiąc się o stosy dokumentów.
+            </p>
 
-          <p className="mainContent">
-            <br />
-            <p style={{fontWeight: 'bold'}}> Pomoc w legalizacji pracy </p>
-            <br />
+            <img className="networkImg " src={idea} />
+          </div>
+          <div>
+            <p className="mainContent">
+              <br />
+              <p style={{fontWeight: 'bold'}}> Pomoc w legalizacji pracy </p>
+              <br />
 
-            Najważniejszą rzeczą podczas rekrutacji w szególności tej owocnej jest podpisanie
-            stosownej umowy z kandydatem. Ten proces również został zautomatyzowany poprzez
-            zastosowanie systemu “Thru-Sync”, który umożliwia przesłanie umowy bezpośrednio do
-            kandydata.
-          </p>
+              Najważniejszą rzeczą podczas rekrutacji w szególności tej owocnej jest podpisanie
+              stosownej umowy z kandydatem. Ten proces również został zautomatyzowany poprzez
+              zastosowanie systemu “Thru-Sync”, który umożliwia przesłanie umowy bezpośrednio do
+              kandydata.
+            </p>
+            <img className="networkImg " src={help} />
+          </div>
         </div>
 
         <div>
