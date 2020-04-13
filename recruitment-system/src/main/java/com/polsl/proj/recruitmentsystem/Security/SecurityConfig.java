84214fcf7b25c672916f7d780c61333cde6833e6
KamilSusek <dumacataloni@gmail.com>
2020-04-13 11:15:32 +0200

    pusty commit

--- recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/Security/SecurityConfig.java
+++ recruitment-system/src/main/java/com/polsl/proj/recruitmentsystem/Security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.authentication.AuthenticationManagerBeanDefinitionParser;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/favicon",
             "/register"
     };
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
