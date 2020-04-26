6481e4386fa72dad5f05ad5c0ec0edb6783fb7fe
wiru123321 <wojtekmaj2@o2.pl>
2020-04-26 16:36:52 +0200

    Filtrowanie aplikacji ciag dalszy.

--- webapp/recruitment-system-web/src/js/recruiter/FindRecruitForm.js
+++ webapp/recruitment-system-web/src/js/recruiter/FindRecruitForm.js
@@ -1,12 +1,67 @@
 import React from 'react';
 
+class Json {
+  json = [
+    {
+      recruit: {
+        firstName: '',
+        lastName: '',
+        educations: [''],
+        empolymentExperiences: [{dateFrom: '', dateTo: '', postion: ''}],
+        skills: [{skillName: '', skillLevel: ''}],
+        trainings: [{name: '', description: '', date: ''}],
+      },
+    },
+  ];
+
+  filterByFirstName (firstName) {}
+
+  filterByLastName (lastName) {}
+
+  filterByEducation (education) {}
+
+  filterByCourseName (courseName) {}
+
+  filterByJobName (jobName) {}
+}
+
+class Filters extends React.Component {
+  render () {
+    return (
+      <div>
+        <input type="text" placeholder="Odbyte zawody" />
+        <input type="text" placeholder="Kurs" />
+        <input type="text" placeholder="Wykształcenie" />
+      </div>
+    );
+  }
+}
+
 class FindRecruitForm extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      showFilters: false,
+    };
+  }
+
   render () {
     return (
       <div>
         <p>Szukaj</p>
         <input type="text" placeholder="imie" />
         <input type="text" placeholder="nazwisko" />
+        <button
+          onClick={event => {
+            event.preventDefault ();
+            this.setState ({showFilters: !this.state.showFilters});
+          }}
+        >
+          Zaawansowane filtry
+        </button>
+        {this.state.showFilters && <Filters />}
+
+        <button>Filtruj</button>
       </div>
     );
   }
