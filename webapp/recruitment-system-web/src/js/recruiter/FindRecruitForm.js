43b5adb3fa407b194e21128a7f927288d99861a0
wiru123321 <wojtekmaj2@o2.pl>
2020-04-26 15:52:15 +0200

    Formularz filtrowania aplikacji po stronie rekrutera.

--- webapp/recruitment-system-web/src/js/recruiter/FindRecruitForm.js
+++ webapp/recruitment-system-web/src/js/recruiter/FindRecruitForm.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class FindRecruitForm extends React.Component {
   render () {
-    return <div><p>FindRecruitForm</p></div>;
+    return (
+      <div>
+        <p>Szukaj</p>
+        <input type="text" placeholder="imie" />
+        <input type="text" placeholder="nazwisko" />
+      </div>
+    );
   }
 }
 
