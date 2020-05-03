d35612d2350106d6f258eb727e9ebf8348646024
besrei <tomek.wienchor@gmail.com>
2020-05-03 17:54:44 +0200

    Dodano arkusz stylow do formularza.

--- webapp/recruitment-system-web/src/js/admin/add-workers/context/AddWorkersContext.js
+++ webapp/recruitment-system-web/src/js/admin/add-workers/context/AddWorkersContext.js
@@ -1,4 +1,5 @@
-import React, {createContext} from 'react';
+import React, {createContext, useContext, useState} from 'react';
+import '../../../../css/AddWorkersForm.css';
 
 export const AddWorkersContext = createContext ();
 
@@ -13,21 +14,80 @@ const AddWorkersContextProvider = () => {
 };
 
 const AddWorkerForm = () => {
+  const [firstName, setFirstName] = useState ('');
+  const [lastName, setLastName] = useState ('');
+  const [password, setPassword] = useState ('');
+  const [rewritePassword, setRewritePassword] = useState ('');
+  const [type, setType] = useState ('');
+
   return (
-    <div>
+    <div className="add-worker-form">
       <form>
-        <input placeholder="Imie" name="firstName" />
-        <input placeholder="Nazwisko" name="lastName" />
-        <input placeholder="Hasło" name="password" />
-        <input placeholder="Powtórz hasło" name="rewritePassword" />
-        <select name="type">
+        <input
+          className="type-field"
+          placeholder="Imie"
+          name="firstName"
+          onChange={event => {
+            setFirstName (event.target.value);
+          }}
+        />
+        <input
+          className="type-field"
+          placeholder="Nazwisko"
+          name="lastName"
+          onChange={event => {
+            setLastName (event.target.value);
+          }}
+        />
+        <input
+          className="type-field"
+          placeholder="Hasło"
+          name="password"
+          onChange={event => {
+            setPassword (event.target.value);
+          }}
+        />
+        <input
+          className="type-field"
+          placeholder="Powtórz hasło"
+          name="rewritePassword"
+          onChange={event => {
+            setRewritePassword (event.target.value);
+          }}
+        />
+        <select
+          className="type-field"
+          name="type"
+          onChange={event => {
+            setType (event.target.value);
+          }}
+        >
           <option value="">-</option>
           <option value="RECRUITER">Rekruter</option>
           <option value="HEAD">Kierownik</option>
         </select>
         <button
+          className="submit-field"
           onClick={event => {
             event.preventDefault ();
+            let worker = {
+              firstName: firstName,
+              lastName: lastName,
+              password: password,
+              rewritePassword: rewritePassword,
+              type: type,
+            };
+            if (
+              firstName !== '' &&
+              lastName !== '' &&
+              password !== '' &&
+              rewritePassword !== '' &&
+              type !== ''
+            ) {
+              if (password === rewritePassword) {
+                console.log ('Worker', worker);
+              }
+            }
           }}
         >
           DODAJ
