a25e997cfae20437d5c78e4bf447351d3ae9ade4
KamilSusek <dumacataloni@gmail.com>
2020-05-30 12:04:20 +0200

    Fix select'ów wprowadzających dane o wykształceniu i poziomie umiejętności.

--- webapp/recruitment-system-web/src/js/recruiter/register-form/Skills.jsx
+++ webapp/recruitment-system-web/src/js/recruiter/register-form/Skills.jsx
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Skills = props => {
+const Skills = (props) => {
   const id = props.id;
-  const [skillName, setSkillName] = useState ('');
-  const [skillLevel, setSkillLevel] = useState ('');
+  const [skillName, setSkillName] = useState("");
+  const [skillLevel, setSkillLevel] = useState("poczatkujacy");
 
   return (
     <div>
       <input
-        onChange={event => {
-          setSkillName (event.target.value);
-          props.onUpdate (id, event.target.value, skillLevel);
+        onChange={(event) => {
+          setSkillName(event.target.value);
+          props.onUpdate(id, event.target.value, skillLevel);
         }}
         name="skillName"
         placeholder="Umiejętność"
@@ -18,38 +18,32 @@ const Skills = props => {
         required
       />
       <select
-        onChange={event => {
-          setSkillLevel (event.target.value);
-          props.onUpdate (id, skillName, event.target.value);
+        onChange={(event) => {
+          setSkillLevel(event.target.value);
+          props.onUpdate(id, skillName, event.target.value);
         }}
         name="skillLevel"
         placeholder="Poziom"
         value={skillLevel}
         required
       >
-        <option value="poczatkujacy">
-          poczatkujacy
-        </option>
-        <option value="srednio-zaawansowany">
-          srednio-zaawansowany
-        </option>
-        <option value="zaawansowany">
-          zaawansowany
-        </option>
+        <option value="poczatkujacy">poczatkujacy</option>
+        <option value="srednio-zaawansowany">srednio-zaawansowany</option>
+        <option value="zaawansowany">zaawansowany</option>
         <option value="ekspert">ekspert</option>
       </select>
       <button
-        onClick={event => {
-          event.preventDefault ();
-          props.onAdd ();
+        onClick={(event) => {
+          event.preventDefault();
+          props.onAdd();
         }}
       >
         DODAJ
       </button>
       <button
-        onClick={event => {
-          event.preventDefault ();
-          props.onRemove (id);
+        onClick={(event) => {
+          event.preventDefault();
+          props.onRemove(id);
         }}
       >
         USUŃ
